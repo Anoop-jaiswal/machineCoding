af3b08fb6d11d5df4bfc7cd7abf6b1b6fcae9995
ANOOP <connect.anoopjaiswal@gmail.com>
2025-07-22 16:22:16 +0530

    counter hooks question

---
+++ src/Questions/Counter.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const useCounter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const decrement = () => {
+    if (count !== 0) {
+      setCount((prev) => prev - 1);
+    }
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return { count, decrement, increment, reset };
+};
+
+const Counter = () => {
+  const { count, decrement, increment, reset } = useCounter();
+  return (
+    <div>
+      <div>{count}</div>
+      <button onClick={increment}>Increment +</button>
+      <button onClick={decrement}>Decrement -</button>
+      <button onClick={reset}>Reset 0</button>
+    </div>
+  );
+};
+
+export default Counter;
